36d203b5469e4c3a05293b661759727354d6382f
johnmilimo <johnwanja@gmail.com>
2018-08-09 15:31:21 +0300

    launchpad set

---
+++ src/main/java/com/example/jms/JmsApplication.java
@@ -0,0 +1,12 @@
+package com.example.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsApplication.class, args);
+	}
+}
